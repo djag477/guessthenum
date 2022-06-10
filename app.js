e473d5ef39595609d8f1189c2a4ea8ad964a4a27
djag477 <101579022+djag477@users.noreply.github.com>
2022-06-10 11:29:43 +0200

    fix dynamic range and add first hints on page

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ let targetNum = Math.floor(Math.random() * 100);
 console.log(targetNum);
 let attempts = 3//Number(document.getElementById("att").textContent)
 console.log(attempts);
+let subHeadline = document.querySelector('.container h3')
+let startRange = document.querySelectorAll('header h1 span')[0]
+let endRange = document.querySelectorAll('header h1 span')[1]
+console.log(subHeadline);
+console.log("start range",startRange);
+console.log("end range",endRange);
 
 let inputBox = document.getElementById("inp")
 const button = document.getElementById("btn")
@@ -14,32 +20,30 @@ button.addEventListener("click", () => {
     if (userInput == targetNum) {
     
         console.log("success!");
+        subHeadline.textContent = "success!"
         inputBox.remove()
         
     
     } else if (userInput > targetNum) {
     
+        subHeadline.textContent = "Aim lower"
+        endRange.textContent = userInput
         console.log("Aim lower");
         attempts--
         
         document.getElementById("att").textContent = attempts;
-        switch (attempts) {
-            case 0:
-                inputBox.remove()
-                break;
-        }
+        if (attempts == 0) inputBox.remove();
+        
     
     } else if (userInput < targetNum) {
     
+        subHeadline.textContent = "Aim higher";
+        startRange.textContent = userInput
         console.log("Aim higher");
         attempts--
         
         document.getElementById("att").textContent = attempts;
-        switch (attempts) {
-            case 0:
-                inputBox.remove()
-                break;
-        }
+        if (attempts == 0) inputBox.remove();
     
     
     } 
